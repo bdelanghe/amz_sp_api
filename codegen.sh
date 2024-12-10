70f1a93eff538b7ea7db842bdde1806d2ef502d4
Robert DeLanghe <1240090+bdelanghe@users.noreply.github.com>
2024-12-10 15:40:39 -0500

    sometimes the dir isn't there

--- codegen.sh
+++ codegen.sh
@@ -7,7 +7,7 @@ FILE='../selling-partner-api-models/models/fulfillment-inbound-api-model/fulfill
 API_NAME=`echo $FILE | awk -F/ '{print $4}'`V0
 MODULE_NAME=`echo $API_NAME | perl -pe 's/(^|-)./uc($&)/ge;s/-//g'`V0
 
-rm -r lib/${API_NAME}
+rm -rf lib/${API_NAME}
 mkdir lib/$API_NAME
 cp config.json lib/$API_NAME
 sed -i '' "s/GEMNAME/${API_NAME}/g" lib/${API_NAME}/config.json
@@ -17,5 +17,5 @@ swagger-codegen generate -i $FILE -l ruby -c lib/${API_NAME}/config.json -o lib/
 
 mv lib/${API_NAME}/lib/${API_NAME}.rb lib/
 mv lib/${API_NAME}/lib/${API_NAME}/* lib/${API_NAME}
-rm -r lib/${API_NAME}/lib
+rm -rf lib/${API_NAME}/lib
 rm lib/${API_NAME}/*.gemspec
