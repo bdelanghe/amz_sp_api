6611e6da5402830ce620168fa2b74785656ccbdf
Robert DeLanghe <1240090+bdelanghe@users.noreply.github.com>
2026-01-16 11:29:04 -0500

    added special case for V0 fulfillment-inbound-api endpoint

--- codegen.sh
+++ codegen.sh
@@ -7,6 +7,14 @@ for FILE in `find ../selling-partner-api-models/models -name "*.json"`; do
 	API_NAME=`echo $FILE | awk -F/ '{print $4}'`
 	MODULE_NAME=`echo $API_NAME | perl -pe 's/(^|-)./uc($&)/ge;s/-//g'`
 
+	# Fulfillment Inbound API v0 is mostly deprecated, but some required
+	# inbound operations still exist only in v0 per Amazon's docs:
+ 	# https://developer-docs.amazon.com/sp-api/docs/fulfillment-inbound-api#considerations
+   	if [[ "$FILE" == *"fulfillment-inbound-api-model/fulfillmentInboundV0.json" ]]; then
+		API_NAME="${API_NAME}-V0"
+		MODULE_NAME="${MODULE_NAME}V0"
+	fi
+
 	# use -f to avoid erroring if directory doesn't exist
 	rm -rf lib/${API_NAME}
 	mkdir lib/$API_NAME
